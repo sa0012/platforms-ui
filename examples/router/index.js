3f23fd8f917f267fce6b0b94a670a5867f3c6a87
chenlong <1583479514@qq.com>
2019-12-14 11:31:38 +0800

    cache

--- examples/router/index.js
+++ examples/router/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import hljs from 'highlight.js'
-import BinUI from '../../src/index'
+// import BinUI from '../../src/index'
 // 路由数据
 import routes from './routes'
 
-Vue.use(BinUI)
+// Vue.use(BinUI)
 Vue.use(VueRouter)
 
 // 导出路由 在 main.js 里使用
@@ -15,16 +15,16 @@ const router = new VueRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  BinUI.LoadingBar.start()
-  next()
-})
-router.afterEach(() => {
-  Vue.nextTick(() => {
-    const blocks = document.querySelectorAll('pre code:not(.hljs)')
-    Array.prototype.forEach.call(blocks, hljs.highlightBlock)
-  })
-  BinUI.LoadingBar.done()
-})
+// router.beforeEach((to, from, next) => {
+//   BinUI.LoadingBar.start()
+//   next()
+// })
+// router.afterEach(() => {
+//   Vue.nextTick(() => {
+//     const blocks = document.querySelectorAll('pre code:not(.hljs)')
+//     Array.prototype.forEach.call(blocks, hljs.highlightBlock)
+//   })
+//   BinUI.LoadingBar.done()
+// })
 
 export default router
